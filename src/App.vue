479da2792ce0533deb624fe4e2eb26c10d6241d0
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2023-01-02 17:43:07 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,64 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-spacer />
+      <span class="header">P-Okul</span>
+      <v-spacer />
+    </v-app-bar>
+    <v-card height="100vh" elevation="0">
+      <v-main>
+        <transition name="fade" mode="out-in">
+          <router-view />
+        </transition>
+      </v-main>
+    </v-card>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+  border-radius: 20;
+}
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+body {
+  font-family: "Readex Pro";
+}
+.scroll {
+  overflow-y: scroll;
+}
+.header {
+  font-size: 25px;
 }
 </style>
