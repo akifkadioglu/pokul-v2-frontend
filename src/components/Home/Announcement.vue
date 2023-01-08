45602db3feefa56b2167f07cc49b5a7c83b34dd1
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2023-01-08 19:17:11 +0300

    fixed something

--- src/components/Home/Announcement.vue
+++ src/components/Home/Announcement.vue
@@ -1,22 +1,20 @@
 <template>
-    
-    <v-carousel
-      show-arrows-on-hover
-      :prev-icon="$icons.BACK"
-      :next-icon="$icons.NEXT"
-      :delimiter-icon="$icons.LINE"
-      height="300"
-      hide-delimiter-background
-    >
-      <v-carousel-item
-        contain
-        max-height="300"
-        v-for="(item, i) in items"
-        :key="i"
-        :src="item.src"
-      ></v-carousel-item>
-    </v-carousel>
-   
+  <v-carousel
+    show-arrows-on-hover
+    :prev-icon="$icons.BACK"
+    :next-icon="$icons.NEXT"
+    :delimiter-icon="$icons.LINE"
+    height="300"
+    hide-delimiter-background
+  >
+    <v-carousel-item
+      contain
+      max-height="300"
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
@@ -41,6 +39,4 @@ export default {
   },
 };
 </script>
-<style>
-
-</style>
+<style></style>
