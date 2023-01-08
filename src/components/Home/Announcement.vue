131bd086ffe968f85878584c9a3e884097d4555e
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2023-01-08 19:22:46 +0300

    trying to deploy (500mb error)

--- src/components/Home/Announcement.vue
+++ src/components/Home/Announcement.vue
@@ -1,20 +1,5 @@
 <template>
-  <v-carousel
-    show-arrows-on-hover
-    :prev-icon="$icons.BACK"
-    :next-icon="$icons.NEXT"
-    :delimiter-icon="$icons.LINE"
-    height="300"
-    hide-delimiter-background
-  >
-    <v-carousel-item
-      contain
-      max-height="300"
-      v-for="(item, i) in items"
-      :key="i"
-      :src="item.src"
-    ></v-carousel-item>
-  </v-carousel>
+  <div>sdfsdfsdf</div>
 </template>
 
 <script>
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ],
-  build: {
-    babel: {
-      compact: true,
-    },
-  },
+  ]
 }
