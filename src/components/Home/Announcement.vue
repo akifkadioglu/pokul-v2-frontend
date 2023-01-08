c4c9fb8da882cb4423f3de2ec6b824f3e07ced55
akif kadıoğlu <akif.kadioglu.28@gmail.com>
2023-01-08 22:52:18 +0300

    added ambient mode

--- src/components/Home/Announcement.vue
+++ src/components/Home/Announcement.vue
@@ -4,21 +4,32 @@
     :prev-icon="$icons.BACK"
     :next-icon="$icons.NEXT"
     :delimiter-icon="$icons.LINE"
-    height="400"
+    height="500"
     hide-delimiter-background
   >
-    <v-carousel-item
-      contain
-      max-height="400"
-      v-for="(item, i) in items"
-      :key="i"
-      :src="item.src"
-    ></v-carousel-item>
+    <v-carousel-item eager v-for="(item, i) in items" :key="i">
+      <v-row class="fill-height" align="center" justify="center">
+        <div class="text-h2">
+          <ambient-image
+            :elevation="18"
+            :blur="20"
+            :height="350"
+            :src="item.src"
+          />
+        </div>
+      </v-row>
+      <v-row justify="center"> </v-row>
+    </v-carousel-item>
   </v-carousel>
 </template>
 
 <script>
+import AmbientImage from "ambient-image";
+
 export default {
+  components: {
+    AmbientImage,
+  },
   data() {
     return {
       items: [
@@ -26,17 +37,22 @@ export default {
           src: "https://www.p-okul.net/socialmedia.png",
         },
         {
-          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          src: "https://ocakmedya.com/wp-content/uploads/2022/10/0x0-dunyadan-muhtesem-doga-manzaralari-1512736354182.jpg",
         },
         {
-          src: "https://www.p-okul.net/socialmedia.png",
+          src: "https://i.pinimg.com/originals/a0/c2/2b/a0c22b34dacb4d2c76bd0fe27f833a2c.jpg",
         },
         {
-          src: "https://www.p-okul.net/socialmedia.png",
+          src: "https://bigumigu.com/wp-content/uploads/2017/09/31537399559c38192841818.44166240_n.jpg",
         },
       ],
     };
   },
 };
 </script>
-<style></style>
+<style scoped>
+.ambientimage {
+  object-fit: contain;
+  height: 400px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import i18n from './i18n'
 import VueCryptojs from 'vue-cryptojs'
-
+import AmbientImage from 'ambient-image'
+import '../node_modules/ambient-image/dist/lib/ambient-image.min.css'
 import 'material-icons/iconfont/material-icons.css';
 
 import { variables } from './variables'
@@ -29,5 +30,6 @@ new Vue({
   vuetify,
   i18n,
   VueCryptojs,
+  AmbientImage,
   render: h => h(App)
 }).$mount('#app')
